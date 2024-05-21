31016a7482a977b0584ea9c3e40f0bb7ce50b28f
ninholii <adilson.carvalho@etec.sp.gov.br>
2024-05-21 17:31:56 -0300

    Animations for home page

--- Vitrine.Online.WebApp/wwwroot/js/Components/carousel.js
+++ Vitrine.Online.WebApp/wwwroot/js/Components/carousel.js
@@ -1,4 +1,38 @@
-﻿$(document).ready(() => {
+﻿function debounce(func, wait, immediate) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
 
 
-});
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll() {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    target.forEach(function (element) {
+        if ((windowTop) > element.offsetTop) {
+            element.classList.add(animationClass);
+        }
+        else {
+            element.classList.remove(animationClass);
+        }
+    })
+}
+
+animeScroll();
+
+if (target.length) {
+    window.addEventListener('scroll', debounce(function () {
+        animeScroll();
+    }, 50));
+}
